2a961013af4fe9853a2927f2dea92fc4c88ea5bc
amntbp <hassanianes0@gmail.com>
2025-07-28 14:44:48 +0100

    nvim setup J's one + cp

---
+++ nvim/lua/j/snippets/cpp.lua
@@ -0,0 +1,13 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+  s("!cp", {
+    t({ "#include <bits/stdc++.h>", "using namespace std", "","int main() {", "  ios_base::sync_with_stdio(false);", "  cin.tie(nullptr);", "", "  " }),
+    i(1,""),
+    t({ "", "}" }),
+  }),
+}
+
